9f0011cbf703819b959d421d39ca2614e5911cbe
student <caici1314520@gmail.com>
2024-05-22 16:38:19 +0800

    v2_楊依姍

--- app/src/main/java/com/example/s1114760/SecondActivity.kt
+++ app/src/main/java/com/example/s1114760/SecondActivity.kt
@@ -1,8 +1,7 @@
 package com.example.s1114760
 
-import android.content.Intent
+import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -10,76 +9,66 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.rounded.AccountBox
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 import com.example.s1114760.ui.theme.S1114760Theme
 
-class MainActivity : ComponentActivity() {
+class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             S1114760Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting2("Android")
                 }
             }
         }
     }
 }
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    Column {
-        Button(onClick = { var it = Intent(context, SecondActivity::class.java)
-            context.startActivity(it)
-        }){
-            Text(text = "主要機構")
-        }
 
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val activity = (context as Activity)
+
+
+    Column {
+        Button(onClick = { activity.finish() })
+        {
+            Text(text = "簡介")
+        }
         Image(
             painter = painterResource(id = R.drawable.maria),
             contentDescription = "圖片",
             alpha = 0.7f,
-            modifier = Modifier
-                .clip(CircleShape)
-                .background(Color.Black)
+            modifier = Modifier.clip(CircleShape).background(Color.Black)
         )
+
         Text(
-            text = "簡介",
-            color = Color.Blue,
+            text = "主要機構",
+            color = Color.Red,
             modifier = modifier
         )
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview2() {
     S1114760Theme {
-        Greeting("Android")
+        Greeting2("Android")
     }
 }
